70fc10196d5ef6aa26aa3029c20a75bf4b3f4a58
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-11-10 03:00:19 +0100

    finally ECS6 classes is great

--- 6_classes.js
+++ 6_classes.js
@@ -1,1 +1,34 @@
 // classes
+class Book{
+    constructor(title,author,year){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+    getbookinfo(){
+        return `${this.title} was written by ${this.author} in ${this.year}`
+    }
+    getage(){
+        const years = new Date().getFullYear() - this.year;
+        return `the book ${this.title} have ${years} years old`
+    }
+    revise(newYear){
+        this.year = newYear;
+        this.revised = true;
+     }
+    // even when i create instance from the class static method does not excute
+    static toppeople(){
+        return 'baba & mama'
+    }
+}
+
+
+// instantiate object
+const book1 = new Book('Book one ', 'john doe','2013')
+
+console.log(book1)
+book1.revise('2019')
+
+console.log(book1); 
+
+console.log(Book.toppeople());
--- 7_subclasses.js
+++ 7_subclasses.js
@@ -1,1 +1,23 @@
 // subclasses
+class Book{
+    constructor(title,author,year){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+    getbookinfo(){
+        return `${this.title} was written by ${this.author} in ${this.year}`
+    }
+}
+
+// magazine subclass
+class Magazine extends Book{
+    constructor(title,author,year,month){
+        super(title,author,year,month);
+        this.month = month;
+    }
+}
+
+const mag1 = new Magazine('Book one ', 'john doe','2013',"feb")
+
+console.log(mag1.getbookinfo())
