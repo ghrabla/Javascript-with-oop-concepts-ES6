53ee77eee321c1e6ef24f5aebf5da0aa3823ca67
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-11-10 01:34:02 +0100

    end of constructors

--- 2_constructor.js
+++ 2_constructor.js
@@ -1,1 +1,17 @@
 // constructor
+function Book(title,author,year){
+    // console.log('book init ...')
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.getbookinfo = function(){
+        return `${this.title} was written by ${this.author} in ${this.year}`
+    }
+}  
+
+// instatiate an object
+// we can do that M times by one function
+const book3 = new Book("all things js","YTB AJZ","2022");
+const book4 = new Book("good moments","alicia keys","2011");
+ console.log(book3.getbookinfo())  
+ console.log(book4.getbookinfo())  
