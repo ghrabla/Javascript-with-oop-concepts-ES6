dce39e211f33bb0c6fc474ae5b2f5c168ff4c5b5
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-11-10 02:39:40 +0100

    create object function

--- 5_object_create.js
+++ 5_object_create.js
@@ -1,1 +1,24 @@
-// objects
+// object of protos
+const bookProtos = {
+    getbookinfo : function(){
+        return `${this.title} was written by ${this.author} in ${this.year}`
+    },
+    getage : function(){
+        const years = new Date().getFullYear() - this.year;
+        return `the book ${this.title} have ${years} years old`
+    }
+}
+
+// create object
+// const book1 = Object.create(bookProtos);
+// book1.title = 'book one ';
+// book1.author = 'john doe ';
+// book1.title = '2013';
+const book1 = Object.create(bookProtos,{
+    title : {value: 'book one '},
+    author : {value:'kamal ghrabla'},
+    year: {value: '2002'}
+});
+ 
+
+console.log(book1);
